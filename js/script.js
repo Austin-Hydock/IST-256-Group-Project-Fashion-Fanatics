833336776d47070c98dcd92957850bdd02e7a58e
deema6 <deema.miss@gmail.com>
2026-02-27 17:06:55 -0500

    Create script.js
    javascript base

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let users = []; //array for users
+let editIndex = null; //indexin case of edits. if null user is added, if not we will edit the user
+
+//calling submitted for - name pending
+document.getElementById("").addEventListener("submit", function(e) {
+    e.preventDefault(); //no auto refresh
+    handleFormSubmit();
+});
+
+//function for handling adding or updating user
+function handleFormSubmit() {
+    
+} 
+
+function emailValidation() {
+    
+}
+
