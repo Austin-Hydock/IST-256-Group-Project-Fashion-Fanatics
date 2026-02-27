87fe935f18825cdacd2fd41d9445c2ac4cc36874
deema6 <deema.miss@gmail.com>
2026-02-27 18:27:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-let users = []; //array for users
-let editIndex = null; //indexin case of edits. if null user is added, if not we will edit the user
-
 //calling submitted user info
 document.getElementById("memberForm").addEventListener("submit", function(event) {
     event.preventDefault(); //no auto refresh
@@ -14,6 +11,7 @@ function handleFormSubmit() {
     const age = document.getElementById("age").value.trim();
     const email = document.getElementById("email").value.trim();
     const address = document.getElementById("address").value.trim();
+    let phone = document.getElementById("phone").value.trim();
 
     //validating that something was inputted, alr required in HTML, but adding it here to double check
     if (!name || !age || !email || !address) {
@@ -22,10 +20,13 @@ function handleFormSubmit() {
     }
 
     //calling email validation function
-    if (validateEmail(email)) {
+    if (!emailValidation(email)) {
         alert("Please enter a valid email address.")
         return;
     }
+
+    //placeholder
+    processUserData(name, age, email, address, phone);
 } 
 
 //function for email 
@@ -33,5 +34,10 @@ function emailValidation(email) {
     // pattern must be something@something.something
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
+}
+
+//creating JSON object + saving locally + rendering user list
+function processUserData(name, age, email, address, phone) {
+    
 }
 
