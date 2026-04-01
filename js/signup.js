dee703cf1cbc1e9185f461e0e261e83855dfd770
deema6 <deema.miss@gmail.com>
2026-04-01 09:35:26 -0400

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -45,7 +45,8 @@ function processUserData(name, age, email, address, phone) {
 
     localStorage.setItem("users", JSON.stringify(users));
 
-    renderUserList(users);
+    document.getElementById("memberForm").reset();
+
 }
 
 function renderUserList(users) {
