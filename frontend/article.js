fd8aa76d4cf630bf0f8ae86238f0428030c3a26f
jas9814 <jas9814@psu.edu>
2026-04-11 19:20:44 -0400

    Update article.js

--- frontend/article.js
+++ frontend/article.js
@@ -1,14 +1,17 @@
-async function loadProducts() {
-    const res = await fetch("http://localhost:3000/products");
-    const data = await res.json();
+async function loadArticles() {
+    const res = await fetch("http://localhost:3000/articles");
+    const articles = await res.json();
 
-    const container = document.getElementById("productList");
+    const container = document.getElementById("articleList");
     container.innerHTML = "";
 
-    data.forEach(p => {
+    articles.forEach(a => {
         container.innerHTML += `
-            <h3>${p.name}</h3>
-            <p>$${p.price}</p>
+            <h2>${a.title}</h2>
+            <p>${a.content}</p>
+            <hr>
         `;
     });
 }
+
+loadArticles();
