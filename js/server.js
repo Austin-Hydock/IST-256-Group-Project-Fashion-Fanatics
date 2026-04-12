30b42c9806cb5f8657790590f5000f3916ffe4e7
deema6 <deema.miss@gmail.com>
2026-04-11 23:44:52 -0400

    Update server.js

--- js/server.js
+++ js/server.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "YOUR_PASSWORD",
+    password: "root1234",
     database: "fashion_fanatics"
 });
 
